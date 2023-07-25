c3f59ec556db949e0e018ba58cf245821d27ae41
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-07-24 22:02:00 -0400

    can delete and update plant data

--- app/api/plants_routes.py
+++ app/api/plants_routes.py
@@ -17,3 +17,27 @@ def handle_plants():
     print('data from request ================> ', data)
     plants_collection.insert_one(data)
     return jsonify({'message': 'Plant created successfully'})
+
+
+@plants_routes.route('/<string:_id>', methods=['PUT', 'DELETE'])
+def handle_single_user(_id):
+    print('id of the single user  ==== = = = >', _id)
+
+
+    if request.method == 'DELETE':
+        plant = plants_collection.find_one_and_delete({"_id": ObjectId(_id)})  # Use find_one() to get a single document
+
+    if request.method == 'PUT':
+        data = request.get_json()
+        plant = plants_collection.find_one_and_replace({"_id": ObjectId(_id)}, data)  # Use find_one() to get a single document
+        print('data ====> ', data)
+        print('plant ======> ', plant)
+
+    print('plant detail from query ====> ', plant)
+
+
+    if plant:
+        plant['_id'] = str(plant['_id'])  # Convert the '_id' field to a string
+        return jsonify(plant)
+    else:
+        return jsonify({'message': 'plant not found'}), 404
