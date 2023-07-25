be15bba3096cf0f5b5c7ad342d1d0bd2bf4b7bbf
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-07-24 22:19:22 -0400

    accessing plants in database

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,12 +5,15 @@ import SignupFormPage from "./components/SignupFormPage";
 import LoginFormPage from "./components/LoginFormPage";
 import { authenticate } from "./store/session";
 import Navigation from "./components/Navigation";
+import { fetchPlants } from "./store/plants";
 
 function App() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
   useEffect(() => {
     dispatch(authenticate()).then(() => setIsLoaded(true));
+    dispatch(fetchPlants())
+
   }, [dispatch]);
 
   return (
