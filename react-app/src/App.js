0d57d6b09dcf852761fd80e4a8cb8247ef463c64
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-08-09 11:08:39 -0400

    f

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -8,6 +8,9 @@ import { fetchPlants } from "./store/plants";
 import Plants from "./components/Plants";
 import SignupFormPage from "./components/SignupFormPage";
 import Home from "./components/LoginFormModal/Home";
+import { Navbar, Nav } from 'rsuite';
+import HomeIcon from '@rsuite/icons/legacy/Home';
+import CogIcon from '@rsuite/icons/legacy/Cog';
 
 function App() {
   const dispatch = useDispatch();
@@ -20,7 +23,25 @@ function App() {
 
   return (
     <>
-
+      <Navbar>
+        <Navbar.Brand href="#">RSUITE</Navbar.Brand>
+        <Nav>
+          <Nav.Item icon={<HomeIcon />}>Home</Nav.Item>
+          <Nav.Item>News</Nav.Item>
+          <Nav.Item>Products</Nav.Item>
+          <Nav.Menu title="About">
+            <Nav.Item>Company</Nav.Item>
+            <Nav.Item>Team</Nav.Item>
+            <Nav.Menu title="Contact">
+              <Nav.Item>Via email</Nav.Item>
+              <Nav.Item>Via telephone</Nav.Item>
+            </Nav.Menu>
+          </Nav.Menu>
+        </Nav>
+        <Nav pullRight>
+          <Nav.Item icon={<CogIcon />}>Settings</Nav.Item>
+        </Nav>
+      </Navbar>
       {isLoaded && (
         <Switch>
           <Route exact path='/'>
